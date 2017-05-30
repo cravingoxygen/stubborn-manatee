e159b815c125b4f7af45b82b41ee456c076d922d
elre <vze.ezv@gmail.com>
2017-05-30 12:29:26 +0200

    A simple hello world server

---
+++ server/server.go
@@ -0,0 +1,44 @@
+/*
+Copyright [2017] [Elre Oldewage]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+*/
+
+//Package main registers an HTTP handler at "/" that handles incoming requests for stubborn-manatee.
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+//Every handler should be wrapped by this error handler
+func errorHandler(f func(http.ResponseWriter, *http.Request) error) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := f(w, r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			log.Printf("Handling %q: %v", r.RequestURI, err)
+		}
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) error {
+	io.WriteString(w, "Hello world")
+	return nil
+}
+
+func main() {
+	http.HandleFunc("/", errorHandler(helloHandler))
+	http.ListenAndServe(":8080", nil)
+}
